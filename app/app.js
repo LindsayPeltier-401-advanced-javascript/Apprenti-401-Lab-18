0d5d435794cbbbdd314c958264783dc507b330c6
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-29 11:30:11 -0800

    demo code

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@
 const io = require('socket.io-client');
 
 const socket = io.connect('http://localhost:3000');
+const weather = io.connect('http://localhost:3000/weather');
 
-socket.emit('sunrise');
-socket.emit('sunset');
+socket.emit('sunrise', 'sunrise');
+socket.emit('sunset', 'sunset');
 
-socket.emit('rain');
-socket.emit('snow');
+
+weather.emit('rain', 'rain');
+weather.emit('snow', 'snow');
